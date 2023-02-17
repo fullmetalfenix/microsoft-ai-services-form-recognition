286f83543e7109d9037eea0ccd4e5b8ee7f1eee5
Mr <jpihlk@yahoo.com>
2023-02-16 22:11:45 -0500

    inital commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const { AzureKeyCredential, DocumentAnalysisClient } = require("@azure/ai-form-recognizer");
+
+  // set `<your-key>` and `<your-endpoint>` variables with the values from the Azure portal.
+  const key = "<your-key>";
+  const endpoint = "<your-endpoint>";
+
+  // sample document
+  const formUrl = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/sample-layout.pdf"
+
+  async function main() {
+    const client = new DocumentAnalysisClient(endpoint, new AzureKeyCredential(key));
+
+    const poller = await client.beginAnalyzeDocumentFromUrl("prebuilt-document", formUrl);
+
+    const {keyValuePairs} = await poller.pollUntilDone();
+
+    if (!keyValuePairs || keyValuePairs.length <= 0) {
+        console.log("No key-value pairs were extracted from the document.");
+    } else {
+        console.log("Key-Value Pairs:");
+        for (const {key, value, confidence} of keyValuePairs) {
+            console.log("- Key  :", `"${key.content}"`);
+            console.log("  Value:", `"${(value && value.content) || "<undefined>"}" (${confidence})`);
+        }
+    }
+
+}
+
+main().catch((error) => {
+    console.error("An error occurred:", error);
+    process.exit(1);
+});
